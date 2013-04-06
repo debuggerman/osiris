c75088c01192a8e4114382346bb05d973ea9f247
Muhammad Zaki Shaheen <zaki.shaheen@coeus-solutions.de>
2013-04-06 22:33:46 +0500

    added leave request + dateutil module

--- coeus-attendence/controllers/LeaveRequestHandler.py
+++ coeus-attendence/controllers/LeaveRequestHandler.py
@@ -5,6 +5,8 @@ from google.appengine.ext import webapp
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 import webapp2
+from models.models import *
+from dateutil.parser import *
 
 class LeaveRequestHandler(webapp2.RequestHandler):
 
@@ -15,9 +17,9 @@ class LeaveRequestHandler(webapp2.RequestHandler):
 			loginUrl = users.create_login_url(self.request.path)
 			self.redirect(loginUrl)
 			return
-			
+
 		path =  os.path.join(os.path.dirname(__file__),'../views', 'leaveRequest.html')
-		self.response.write(template.render(path, None))
+		self.response.write(template.render(path, {"current_date": datetime.now()}))
 
 
 
@@ -30,12 +32,34 @@ class LeaveRequestHandler(webapp2.RequestHandler):
 			self.redirect(login_url)
 			return
 
-		to_addr = self.request.get("leaveApproverEmail")
+		#leaveStartDate
+		#leaveEndDate
+		#leaveType
+		#leaveApproverEmail
+		#leaveContactNumber
+		#leaveAddressOnLeave
+		leaveStartDate = parse(self.request.get("leaveStartDate"))
+		leaveEndDate = parse(self.request.get("leaveEndDate"))
+		leaveType = self.request.get("leaveType")
+		leaveApproverEmail = self.request.get("leaveApproverEmail")
+		leaveAddressOnLeave = self.request.get("leaveAddressOnLeave")
+		leaveContactNumber = self.request.get("leaveContactNumber")
+		leaveStatus = "pending"
 
-		message = mail.EmailMessage()
-		message.sender = user.email()
-		message.to = to_addr
-		message.body = """
-			Someone requires your approval for the leaves
-		"""
-		message.send()
+		leave = Leave()
+		leave.leaveSubmissionDate = datetime.datetime.now().date()
+		leave.leaveStartDate = leaveStartDate.date()
+		leave.leaveEndDate = leaveEndDate.date()
+		leave.leaveApproverEmail = leaveApproverEmail
+		leave.leaveType = leaveType
+		leave.leaveStatus = leaveStatus
+		leave.put()
+
+		self.response.write("Submited")
+		# message = mail.EmailMessage()
+		# message.sender = user.email()
+		# message.to = to_addr
+		# message.body = """
+		# 	Someone requires your approval for the leaves
+		# """
+		# message.send()
