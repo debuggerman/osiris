7badb7a5da7607d10b6c785d527907c16878a399
Muhammad Zaki Shaheen <zaki.shaheen@coeus-solutions.de>
2013-04-06 23:43:21 +0500

    updated error

--- coeus-attendence/main.py
+++ coeus-attendence/main.py
@@ -21,6 +21,6 @@ app = webapp2.WSGIApplication( [
 	webapp2.Route(r'/', handler=MainHandler, name='home'),
 	webapp2.Route(r'/respond_leave.do/<leave_id:\d+>/<method:\w+>', handler=LeaveResponseHandler, name='leaveResponse'),
 	webapp2.Route('/employee_register.do', handler=RegisterHandler, name="register"),
-	webapp2.Route('/request_leave.do?', handler=LeaveRequestHandler, nae="requestLeave"),
+	webapp2.Route('/request_leave.do?', handler=LeaveRequestHandler, name="requestLeave"),
  ], debug=True)
  
